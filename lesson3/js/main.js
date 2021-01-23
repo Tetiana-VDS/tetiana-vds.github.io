8c5e5ee3ee6c921e81d3b21485d898121d3253fd
Tetiana-VDS <77065769+Tetiana-VDS@users.noreply.github.com>
2021-01-23 19:53:47 +0200

    nav

--- lesson3/js/main.js
+++ lesson3/js/main.js
@@ -1,1 +1,6 @@
+document.getElementById('copyrightYear').innerHTML = new Date().getFullYear();
 
+var today = new Date();
+
+var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+document.getElementById("now").innerHTML = date;
