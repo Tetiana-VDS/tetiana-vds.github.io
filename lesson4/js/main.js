0f6c4ca8f17b0184d4d5081f423291d14a077af3
Tetiana-VDS <77065769+Tetiana-VDS@users.noreply.github.com>
2021-01-30 14:46:23 +0200

    new

--- lesson4/js/main.js
+++ lesson4/js/main.js
@@ -1,1 +1,12 @@
+document.getElementById('copyrightYear').innerHTML = new Date().getFullYear();
 
+
+var today = new Date();
+
+var date = today.getFullYear() + '-' + (today.getMonth("long") + 1) + '-' + today.getDate();
+document.getElementById("now").innerHTML = date;
+
+function toggleMenu() {
+ 
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
