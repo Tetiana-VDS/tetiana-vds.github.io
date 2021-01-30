a0664ac43c9ce23194ecec0761db328853016c2b
Tetiana-VDS <77065769+Tetiana-VDS@users.noreply.github.com>
2021-01-30 21:44:31 +0200

    date

--- lesson4/js/main.js
+++ lesson4/js/main.js
@@ -1,14 +1,8 @@
 document.getElementById('copyrightYear').innerHTML = new Date().getFullYear();
 
-//var d = new Date();
-//var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-//document.getElementById("").innerHTML = months[d.getMonth()]+ today.getDate()+","+today.getFullYear();
-
-var today = new Date();
-
-var date = today.getFullYear() + '-' + (today.getMonth("long") + 1) + '-' + today.getDate();
-document.getElementById("now").innerHTML = date;
-
+var d = new Date();
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+document.getElementById("now").innerHTML = months[d.getMonth()]+" "+ d.getDate()+", " + d.getFullYear();
 function toggleMenu() {
  
     document.getElementById("primaryNav").classList.toggle("hide");
