001d12219effd1393011d3bf8e5e30f84264a3c6
Tetiana-VDS <77065769+Tetiana-VDS@users.noreply.github.com>
2021-02-08 12:27:00 +0200

    new

--- lesson4/js/main.js
+++ lesson4/js/main.js
@@ -1,6 +1,7 @@
 document.getElementById('copyrightYear').innerHTML = new Date().getFullYear();
 
 var d = new Date();
+
 var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 document.getElementById("now").innerHTML = months[d.getMonth()]+" "+ d.getDate()+", " + d.getFullYear();
 function toggleMenu() {
--- lesson5/js/main.js
+++ lesson5/js/main.js
@@ -1,8 +1,9 @@
 document.getElementById('copyrightYear').innerHTML = new Date().getFullYear();
 
 var d = new Date();
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-document.getElementById("now").innerHTML = months[d.getMonth()]+" "+ d.getDate()+", " + d.getFullYear();
+document.getElementById("now").innerHTML = days[d.getDay()]+ ", "+ months[d.getMonth()]+" "+ d.getDate()+", " + d.getFullYear();
 function toggleMenu() {
  
     document.getElementById("primaryNav").classList.toggle("hide");
