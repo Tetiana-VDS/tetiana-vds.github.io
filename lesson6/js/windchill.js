5b582606fb640a00a8932568937d424865cd8ea9
Tetiana-VDS <77065769+Tetiana-VDS@users.noreply.github.com>
2021-02-14 06:36:27 +0200

    final

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,5 +1,7 @@
 
-var t = document.getElementById("temp").value;
-var s = document.getElementById("speed").value;
-var chill = 35.74 + 0.6215 * t - (35.75 * Math.pow(s,0.16)) + (0.4275 * t * Math.pow(s,0.16));
+var t = 46;
+var s = 13;
+var a = Math.pow(s,0.16)
+var chill = 35.74 + 0.6215 * t - (35.75 * a) + (0.4275 * t * a);
+var chill= Math.round(chill);
 document.getElementById("windchill").innerHTML = chill;
