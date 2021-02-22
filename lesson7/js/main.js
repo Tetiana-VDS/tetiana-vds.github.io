77cf197e0a6980abdb1110d34dfad5e89ab5d171
Tetiana-VDS <77065769+Tetiana-VDS@users.noreply.github.com>
2021-02-22 07:29:12 +0200

    last

--- lesson7/js/main.js
+++ lesson7/js/main.js
@@ -15,4 +15,25 @@ function toggleMenu() {
         document.getElementById("Saturday").style.display = "block";
     }
 
+    const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+};
+if('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if(item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
  
