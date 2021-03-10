0a8121cf6fdb7121ab802a7accdf03c909d0ade6
Tetiana-VDS <77065769+Tetiana-VDS@users.noreply.github.com>
2021-03-10 06:46:39 +0200

    start of home

--- lesson9/js/main.js
+++ lesson9/js/main.js
@@ -15,25 +15,42 @@ function toggleMenu() {
         document.getElementById("Saturday").style.display = "block";
     }
 
-    const imgOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px 50px 0px"
-};
-if('IntersectionObserver' in window) {
-  const observer = new IntersectionObserver((items, observer) => {
-    items.forEach((item) => {
-      if(item.isIntersecting) {
-        loadImages(item.target);
-        observer.unobserve(item.target);
-      }
-    });
-  });
-  imagesToLoad.forEach((img) => {
-    observer.observe(img);
-  });
-} else {
-  imagesToLoad.forEach((img) => {
-    loadImages(img);
-  });
-}
- 
+const townDataURL = "https://byui-cit230.github.io/weather/data/towndata.json"
+const towns2get = [
+    "Preston",
+    "Fish Haven",
+    "Soda Springs"
+]
+const city = document.querySelector('.city');
+fetch(townDataURL)
+    .then((response) => {
+        return response.json()
+    })
+    .then((jsonData) => {
+      const towns = jsonData["towns"].filter(item => towns2get.includes(item.name));
+
+        towns.forEach(towns2get => {
+        let town = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let p1 = document.createElement('p1');
+        let p2 = document.createElement('p2');
+        let p3 = document.createElement('p3');
+      let p4 = document.createElement('p4');
+     
+  
+        h2.innerHTML = `${towns2get.name}`;
+      p1.innerHTML = `Motto: ${towns2get.motto}`;
+      p2.innerHTML = `Year Founded: ${towns2get.yearFounded}`;
+        p3.innerHTML = `Population: ${towns2get.currentPopulation}`;
+        p4.innerHTML = `Average Rainfall: ${towns2get.averageRainfall}`;
+        
+  
+       city.append(town);
+        city.append(h2);
+        city.append(p1);
+        city.append(p2);
+       city.append(p3);
+        city.append(p4);
+  
+      });
+    })
