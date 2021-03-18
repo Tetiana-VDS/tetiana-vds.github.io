2e8c9124238bea6864686836c7ea167bd4eef520
Tetiana-VDS <77065769+Tetiana-VDS@users.noreply.github.com>
2021-03-18 06:37:53 +0200

    js

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -1,33 +1,49 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=02d538d3f6e5b1a6ebc01ea46b82bc1d&units=imperial";
+const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=02d538d3f6e5b1a6ebc01ea46b82bc1d&units=imperial";
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
+        let day = 0;
         
+        const current = document.querySelector('#current');
+        current.innerHTML = jsObject.list[0].weather[0].main;
+        const temperature = document.querySelector('#temp');
+        temperature.innerHTML = Math.round(jsObject.list[0].main.temp);
+        const humidity = document.querySelector('#humid');
+        humidity.innerHTML = jsObject.list[0].main.humidity;
+        const windspeed = document.querySelector('#speed')
+        windspeed.innerHTML = jsObject.list[0].wind.speed;
+
+         function windchill() {
+var temperature = parseFloat(document.querySelector('#temp').innerHTML);
+    var windSpeed = parseFloat(document.querySelector('#speed').innerHTML);
+    var chill = Math.round(35.74 + 0.6215 * temperature - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16)));
+
+             document.getElementById("windchill").innerHTML = chill;
+         }
+    });
+
+        const apiURL32 = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=02d538d3f6e5b1a6ebc01ea46b82bc1d&units=imperial";
+fetch(apiURL32)
+    .then((response) => response.json())
+    .then((jsObject) => {
+         console.log(jsObject);
         const thefive = jsObject.list.filter(time => time.dt_txt.includes('18:00:00'));
         console.log(thefive);
         
         thefive.forEach(x => {
-                    let day = 0;
-//         const dayofWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-//             let d = new Date(x.dt_txt);
-//             document.getElementById(`dayofWeek${x + 1}`).textContent = dayofWeek[d.getDay()];
-//                document.getElementById(`forecast${x + 1}`).textContent = x.main.temp;
-//             const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[x].icon + '.png';  // note the concatenation
-// const desc = jsObject.weather[0].description; 
-// document.getElementById('imagesrc').textContent = imagesrc; 
-// document.getElementById(`wicon${x + 1}`).setAttribute('src', imagesrc);  
-// document.getElementById(`wicon${x + 1}`).setAttribute('alt', desc);  
-//             document.getElementById(`forecast${x + 1}`).textContent = x.main.temp;
-
-//             day++
-       const current = document.querySelector('#current');
-            current.innerHTML = x.weather[0].main;
-            const temperature = document.querySelector('#temp');
-            temperature.innerHTML = Math.round(x.main.temp);
-            const humidity = document.querySelector('#humid');
-            humidity.innerHTML = x.main.humidity;
-             const windspeed = document.querySelector('#speed')
-            windspeed.innerHTML = x.wind.speed;
-     })
-  });
+            let day = 0;
+            const dayofWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+            let d = new Date(theFive[day+1].dt_txt);
+             const imagesrc = 'https://openweathermap.org/img/w/' + theFive[day].weather[0].icon + '.png';  // note the concatenation
+            const desc = theFive[day+1].weather[0].description;
+            document.getElementById(`dayofWeek${day + 1}`).textContent = dayofWeek[d.getDay()];
+            document.getElementById(`forecast${day + 1}`).textContent = Math.round(forecast[day+1].main.temp);
+            document.getElementById('imagesrc').textContent = imagesrc;
+            document.getElementById(`wicon${day + 1}`).setAttribute('src', imagesrc);
+            document.getElementById(`wicon${day + 1}`).setAttribute('alt', desc);
+            day++
+        })
+       
+    });
+  
--- lesson10/js/windchill.js
+++
@@ -1,6 +0,0 @@
-
-    var temperature = document.querySelector('#temp');
-    var windSpeed = document.querySelector('#speed');
-    var chill = Math.round(35.74 + 0.6215 * temperature - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16)));
-
-    document.getElementById("windchill").innerHTML = chill;
