5407dcb3a61c216f94733a98d2cee5117bcee2ba
Tetiana-VDS <77065769+Tetiana-VDS@users.noreply.github.com>
2021-03-18 16:40:56 +0200

    again

--- lesson11/js/main.js
+++ lesson11/js/main.js
@@ -15,88 +15,88 @@ function toggleMenu() {
         document.getElementById("Saturday").style.display = "block";
     }
 
-// const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
-// fetch(requestURL)
-//     .then(function (response) {
-//         return response.json();
-//     })
-//     .then(function (jsonObject) {
-//         const towns = jsonObject['towns'];
-//         const prestons = document.querySelector('.prestons');
-//       const prestonfilter = towns.filter(x => x.name == "Preston");
-//       prestonfilter.forEach(prestontown => {
-//              let preston = document.createElement('section');
-//              let h2 = document.createElement('h2');
-//            let p1 = document.createElement('p');
-//         let p2 = document.createElement('p');
-//         let p3 = document.createElement('p');
-// let p4 = document.createElement('p');
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        const towns = jsonObject['towns'];
+        const prestons = document.querySelector('.prestons');
+      const prestonfilter = towns.filter(x => x.name == "Preston");
+      prestonfilter.forEach(prestontown => {
+             let preston = document.createElement('section');
+             let h2 = document.createElement('h2');
+           let p1 = document.createElement('p');
+        let p2 = document.createElement('p');
+        let p3 = document.createElement('p');
+let p4 = document.createElement('p');
           
-//            h2.innerHTML = `${prestontown.name}`;
-//         p1.innerHTML = `<span style="font-weight:900">Motto:${prestontown.motto}</span> `;
-//         p2.innerHTML = `<span style="font-weight:900">Year Founded:</span> ${prestontown.yearFounded}`;
-//         p3.innerHTML = `<span style="font-weight:900">Population:</span> ${prestontown.currentPopulation}`;
-//         p4.innerHTML = `<span style="font-weight:900">Average Rainfall:</span> ${prestontown.averageRainfall}`;
+           h2.innerHTML = `${prestontown.name}`;
+        p1.innerHTML = `<span style="font-weight:900">Motto:${prestontown.motto}</span> `;
+        p2.innerHTML = `<span style="font-weight:900">Year Founded:</span> ${prestontown.yearFounded}`;
+        p3.innerHTML = `<span style="font-weight:900">Population:</span> ${prestontown.currentPopulation}`;
+        p4.innerHTML = `<span style="font-weight:900">Average Rainfall:</span> ${prestontown.averageRainfall}`;
         
-//               prestons.append(preston);
-//         preston.append(h2);
-//            preston.append(p1);
-//               preston.append(p2);
-//         preston.append(p3);
-//         preston.append(p4);
+              prestons.append(preston);
+        preston.append(h2);
+           preston.append(p1);
+              preston.append(p2);
+        preston.append(p3);
+        preston.append(p4);
        
-//       });
+      });
       
-      // const fishHs = document.querySelector('.fishHs');
-      // const fishHfilter = towns.filter(x => x.name == "Fish Haven");
-      // fishHfilter.forEach(fishHtown => {
-      //        let fishH = document.createElement('section');
-      //        let h2 = document.createElement('h2');
-      //      let p1 = document.createElement('p');
-      //   let p2 = document.createElement('p');
-      //    let p3 = document.createElement('p');
-      //      let p4 = document.createElement('p');
+      const fishHs = document.querySelector('.fishHs');
+      const fishHfilter = towns.filter(x => x.name == "Fish Haven");
+      fishHfilter.forEach(fishHtown => {
+             let fishH = document.createElement('section');
+             let h2 = document.createElement('h2');
+           let p1 = document.createElement('p');
+        let p2 = document.createElement('p');
+         let p3 = document.createElement('p');
+           let p4 = document.createElement('p');
           
-      //    let image = document.createElement('img');
+         let image = document.createElement('img');
          
-      //      h2.innerHTML = `${fishHtown.name}`;
-      //      p1.innerHTML = `<span style="font-weight:900">Motto:${fishHtown.motto}</span> `;
-      //   p2.innerHTML = `<span style="font-weight:900">Year Founded:</span> ${fishHtown.yearFounded}`;
-      //    p3.innerHTML = `<span style="font-weight:900">Population:</span> ${fishHtown.currentPopulation}`;
-      //    p4.innerHTML = `<span style="font-weight:900">Average Rainfall:</span> ${fishHtown.averageRainfall}`;
+           h2.innerHTML = `${fishHtown.name}`;
+           p1.innerHTML = `<span style="font-weight:900">Motto:${fishHtown.motto}</span> `;
+        p2.innerHTML = `<span style="font-weight:900">Year Founded:</span> ${fishHtown.yearFounded}`;
+         p3.innerHTML = `<span style="font-weight:900">Population:</span> ${fishHtown.currentPopulation}`;
+         p4.innerHTML = `<span style="font-weight:900">Average Rainfall:</span> ${fishHtown.averageRainfall}`;
          
-      //     image.setAttribute('src', fishHtown.photo);
-      //       image.setAttribute('alt', `${fishHtown.name}`);
+          image.setAttribute('src', fishHtown.photo);
+            image.setAttribute('alt', `${fishHtown.name}`);
         
-      //        fishH.append(h2);
-      //      fishH.append(p1);
-      //         fishH.append(p2);
-      //   fishHs.append(fishH);
-      //   fishH.append(p3);
-      //    fishH.append(p4);
-      //    fishH.append(image);
-      // });
-      //  const sodas = document.querySelector('.sodas');
-      // const sodafilter = towns.filter(x => x.name == "Soda Springs");
-      // sodafilter.forEach(sodatown => {
-      //        let soda = document.createElement('section');
-      //        let h2 = document.createElement('h2');
-      //      let p1 = document.createElement('p');
-      //   let p2 = document.createElement('p');
-      //   let p3 = document.createElement('p');
-      //      let p4 = document.createElement('p');
+             fishH.append(h2);
+           fishH.append(p1);
+              fishH.append(p2);
+        fishHs.append(fishH);
+        fishH.append(p3);
+         fishH.append(p4);
+         fishH.append(image);
+      });
+       const sodas = document.querySelector('.sodas');
+      const sodafilter = towns.filter(x => x.name == "Soda Springs");
+      sodafilter.forEach(sodatown => {
+             let soda = document.createElement('section');
+             let h2 = document.createElement('h2');
+           let p1 = document.createElement('p');
+        let p2 = document.createElement('p');
+        let p3 = document.createElement('p');
+           let p4 = document.createElement('p');
           
-      //      h2.innerHTML = `${sodatown.name}`;
-      //      p1.innerHTML = `<span style="font-weight:900">Motto:${sodatown.motto}</span> `;
-      //   p2.innerHTML = `<span style="font-weight:900">Year Founded:</span> ${sodatown.yearFounded}`;
-      //    p3.innerHTML = `<span style="font-weight:900">Population:</span>${sodatown.currentPopulation}`;
-      //   p4.innerHTML = `<span style="font-weight:900">Average Rainfall:</span> ${sodatown.averageRainfall}`;
+           h2.innerHTML = `${sodatown.name}`;
+           p1.innerHTML = `<span style="font-weight:900">Motto:${sodatown.motto}</span> `;
+        p2.innerHTML = `<span style="font-weight:900">Year Founded:</span> ${sodatown.yearFounded}`;
+         p3.innerHTML = `<span style="font-weight:900">Population:</span>${sodatown.currentPopulation}`;
+        p4.innerHTML = `<span style="font-weight:900">Average Rainfall:</span> ${sodatown.averageRainfall}`;
         
-      //        soda.append(h2);
-      //      soda.append(p1);
-      //         soda.append(p2);
-      //   sodas.append(soda);
-      //   soda.append(p3);
-      //         soda.append(p4);
-      //      });
-      //  });
+             soda.append(h2);
+           soda.append(p1);
+              soda.append(p2);
+        sodas.append(soda);
+        soda.append(p3);
+              soda.append(p4);
+           });
+       });
