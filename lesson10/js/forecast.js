a381e6d622e94f43112ab072351052be3a35d9a6
Tetiana-VDS <77065769+Tetiana-VDS@users.noreply.github.com>
2021-03-18 05:01:06 +0200

    last

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -3,36 +3,31 @@ fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject);
-   
-        let day = 0;
-        const dayofWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
         
         const thefive = jsObject.list.filter(time => time.dt_txt.includes('18:00:00'));
         console.log(thefive);
         
         thefive.forEach(x => {
-            let d = new Date(x.dt_txt);
-            document.getElementById(`dayofWeek${x + 1}`).textContent = dayofWeek[d.getDay()];
-            document.getElementById(`forecast${x + 1}`).textContent = x.main.temp;
-            day++
+                    let day = 0;
+//         const dayofWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+//             let d = new Date(x.dt_txt);
+//             document.getElementById(`dayofWeek${x + 1}`).textContent = dayofWeek[d.getDay()];
+//                document.getElementById(`forecast${x + 1}`).textContent = x.main.temp;
+//             const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[x].icon + '.png';  // note the concatenation
+// const desc = jsObject.weather[0].description; 
+// document.getElementById('imagesrc').textContent = imagesrc; 
+// document.getElementById(`wicon${x + 1}`).setAttribute('src', imagesrc);  
+// document.getElementById(`wicon${x + 1}`).setAttribute('alt', desc);  
+//             document.getElementById(`forecast${x + 1}`).textContent = x.main.temp;
 
-     });
+//             day++
+       const current = document.querySelector('#current');
+            current.innerHTML = x.weather[0].main;
+            const temperature = document.querySelector('#temp');
+            temperature.innerHTML = Math.round(x.main.temp);
+            const humidity = document.querySelector('#humid');
+            humidity.innerHTML = x.main.humidity;
+             const windspeed = document.querySelector('#speed')
+            windspeed.innerHTML = x.wind.speed;
+     })
   });
-
- 
-        //       const current = document.querySelector('#current');
-        //       current.textContent = jsObject.main;
-          
-
-        //   const temperature = document.querySelector(#temp)
-        //   const windchill = document.querySelector(#windchill)
-        //   const dayofweek = document.querySelector(#humid)
-        // humidity.textContent = jsObject.list.main.humidity;
-        //   const dayofweek = document.querySelector(#speed)
-    
-
-//       const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-// const desc = jsObject.weather[0].description;  // note how we reference the weather array
-// document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-// document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-// document.getElementById('icon').setAttribute('alt', desc);  
